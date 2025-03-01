9a764a7217056a729966328a0173f7c047e9aaa2
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2025-03-01 11:57:27 +0800

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -18,7 +18,6 @@ VERSION_ID="${VERSION_ID:-unknown}"
 echo -e "\n========== 系统信息 =========="
 echo "发行版: ${PRETTY_NAME:-$ID}"
 echo "版本号: ${VERSION_ID}"
-echo "包管理器:"
 
 # 确定包管理器
 declare -A PKG_MANAGERS=(
@@ -32,20 +31,19 @@ declare -A PKG_MANAGERS=(
 
 PKG_CMD="${PKG_MANAGERS[$DISTRO]}"
 case $PKG_CMD in
-    apk) echo "Alpine APK" ;;
-    apt) echo "Debian/Ubuntu APT" ;;
-    yum|dnf) echo "RedHat/YUM" ;;
-    *) echo "未知包管理器"; exit 1 ;;
+    apk)   echo "包管理器: Alpine APK" ;;
+    apt)   echo "包管理器: Debian/Ubuntu APT" ;;
+    yum|dnf) echo "包管理器: RedHat/YUM" ;;
+    *)     echo "未知包管理器"; exit 1 ;;
 esac
 
 # 镜像源选择菜单
 echo -e "\n========== 镜像源选择 =========="
-PS3='请选择镜像源 (1-6): '
+PS3='请选择镜像源 (1-5): '
 options=(
     "阿里云"
     "腾讯云" 
     "华为云"
-    "网易"
     "中科大"
     "清华大学"
 )
@@ -54,9 +52,8 @@ select opt in "${options[@]}"; do
         1) MIRROR="ali" ;;
         2) MIRROR="tencent" ;;
         3) MIRROR="huawei" ;;
-        4) MIRROR="163" ;;
-        5) MIRROR="ustc" ;;
-        6) MIRROR="tsinghua" ;;
+        4) MIRROR="ustc" ;;
+        5) MIRROR="tsinghua" ;;
         *) echo "无效选项"; exit 1 ;;
     esac
     break
@@ -69,7 +66,7 @@ case $DISTRO in
         REPO_FILE="/etc/apk/repositories"
         ;;
     debian|ubuntu)
-        CODENAME=${CODENAME:-$(lsb_release -cs 2>/dev/null)}
+        CODENAME=${CODENAME:-$(lsb_release -cs 2>/dev/null || echo "unknown")}
         REPO_FILE="/etc/apt/sources.list"
         ;;
     centos|fedora|rhel)
@@ -88,27 +85,28 @@ backup_file() {
 case $DISTRO in
 alpine)
     case $MIRROR in
-        ali)       URL="http://mirrors.aliyun.com/alpine/" ;;
-        tencent)   URL="https://mirrors.tencent.com/alpine/" ;;
-        huawei)    URL="https://repo.huaweicloud.com/alpine/" ;;
-        163)       URL="http://mirrors.163.com/alpine/" ;;
-        ustc)      URL="http://mirrors.ustc.edu.cn/alpine/" ;;
-        tsinghua)  URL="https://mirrors.tuna.tsinghua.edu.cn/alpine/" ;;
+        ali)      URL="http://mirrors.aliyun.com/alpine/" ;;
+        tencent)  URL="https://mirrors.tencent.com/alpine/" ;;
+        huawei)   URL="https://repo.huaweicloud.com/alpine/" ;;
+        ustc)     URL="http://mirrors.ustc.edu.cn/alpine/" ;;
+        tsinghua) URL="https://mirrors.tuna.tsinghua.edu.cn/alpine/" ;;
     esac
 
     backup_file "$REPO_FILE"
     cat > "$REPO_FILE" <<EOF
 ${URL}v${ALPINE_VERSION}/main
 ${URL}v${ALPINE_VERSION}/community
+${URL}edge/main
+${URL}edge/community
+${URL}edge/testing
 EOF
     ;;
 
-debian|ubuntu)
+debian)
     declare -A MIRROR_URLS=(
         [ali]="http://mirrors.aliyun.com/debian/"
         [tencent]="http://mirrors.tencentyun.com/debian/"
         [huawei]="http://repo.huaweicloud.com/debian/"
-        [163]="http://mirrors.163.com/debian/"
         [ustc]="http://mirrors.ustc.edu.cn/debian/"
         [tsinghua]="https://mirrors.tuna.tsinghua.edu.cn/debian/"
     )
@@ -116,7 +114,6 @@ debian|ubuntu)
         [ali]="http://mirrors.aliyun.com/debian-security/"
         [tencent]="http://mirrors.tencentyun.com/debian-security/"
         [huawei]="http://repo.huaweicloud.com/debian-security/"
-        [163]="http://mirrors.163.com/debian-security/"
         [ustc]="http://mirrors.ustc.edu.cn/debian-security/"
         [tsinghua]="https://mirrors.tuna.tsinghua.edu.cn/debian-security/"
     )
@@ -130,6 +127,24 @@ deb ${SECURITY_URLS[$MIRROR]} $CODENAME-security main contrib non-free
 EOF
     ;;
 
+ubuntu)
+    declare -A MIRROR_URLS=(
+        [ali]="http://mirrors.aliyun.com/ubuntu/"
+        [tencent]="http://mirrors.tencentyun.com/ubuntu/"
+        [huawei]="http://repo.huaweicloud.com/ubuntu/"
+        [ustc]="http://mirrors.ustc.edu.cn/ubuntu/"
+        [tsinghua]="https://mirrors.tuna.tsinghua.edu.cn/ubuntu/"
+    )
+
+    backup_file "$REPO_FILE"
+    cat > "$REPO_FILE" <<EOF
+deb ${MIRROR_URLS[$MIRROR]} $CODENAME main restricted universe multiverse
+deb ${MIRROR_URLS[$MIRROR]} $CODENAME-updates main restricted universe multiverse
+deb ${MIRROR_URLS[$MIRROR]} $CODENAME-backports main restricted universe multiverse
+deb ${MIRROR_URLS[$MIRROR]} $CODENAME-security main restricted universe multiverse
+EOF
+    ;;
+
 centos|fedora|rhel)
     CENTOS_REPO="${REPO_DIR}/CentOS-Base.repo"
     backup_file "$CENTOS_REPO"
@@ -138,14 +153,13 @@ centos|fedora|rhel)
         [ali]="http://mirrors.aliyun.com/centos/\$releasever/os/\$basearch/"
         [tencent]="http://mirrors.tencentyun.com/centos/\$releasever/os/\$basearch/"
         [huawei]="https://repo.huaweicloud.com/centos/\$releasever/os/\$basearch/"
-        [163]="http://mirrors.163.com/centos/\$releasever/os/\$basearch/"
         [ustc]="http://mirrors.ustc.edu.cn/centos/\$releasever/os/\$basearch/"
         [tsinghua]="https://mirrors.tuna.tsinghua.edu.cn/centos/\$releasever/os/\$basearch/"
     )
 
     sed -i -e "s|^mirrorlist=|#mirrorlist=|g" \
            -e "s|^#baseurl=|baseurl=|g" \
-           -e "s|baseurl=.*|baseurl=${BASE_URLS[$MIRROROR]}|g" \
+           -e "s|baseurl=.*|baseurl=${BASE_URLS[$MIRROR]}|g" \
            "$CENTOS_REPO"
     ;;
 
@@ -170,4 +184,4 @@ case $PKG_CMD in
         ;;
 esac
 
-echo -e "\n镜像源已成功更换为 $opt"
+echo -e "\n[完成] 镜像源已成功更换为 $opt"
