badeb96e03b1541c23c80977811f295c53a1dfe0
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2025-03-04 15:12:43 +0800

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -135,7 +135,7 @@ install_compose() {
             apk add --no-cache docker-compose ;;
         *)
             COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
-            curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
+            curl -L "https://add.woskee.nyc.mn/github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
             -o /usr/local/bin/docker-compose
             chmod +x /usr/local/bin/docker-compose
             ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose ;;
