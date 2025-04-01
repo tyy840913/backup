8e007600eecc496ab2527afb1a9007bb7ba430e6
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2025-04-01 11:24:23 +0800

    Create hy2.sh

---
+++ hy2.sh
@@ -0,0 +1,236 @@
+#!/bin/bash
+export LC_ALL=C
+HOSTNAME=$(hostname)
+USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
+export UUID=${UUID:-$(echo -n "$USERNAME+$HOSTNAME" | md5sum | head -c 32 | sed -E 's/(.{8})(.{4})(.{4})(.{4})(.{12})/\1-\2-\3-\4-\5/')}
+export CHAT_ID=${CHAT_ID:-''} 
+export BOT_TOKEN=${BOT_TOKEN:-''} 
+export UPLOAD_URL=${UPLOAD_URL:-''}
+export SUB_TOKEN=${SUB_TOKEN:-${UUID:0:8}}
+
+if [[ "$HOSTNAME" =~ ct8 ]]; then
+    CURRENT_DOMAIN="ct8.pl"
+elif [[ "$HOSTNAME" =~ useruno ]]; then
+    CURRENT_DOMAIN="useruno.com"
+else
+    CURRENT_DOMAIN="serv00.net"
+fi
+WORKDIR="${HOME}/domains/${USERNAME}.${CURRENT_DOMAIN}/logs"
+FILE_PATH="${HOME}/domains/${USERNAME}.${CURRENT_DOMAIN}/public_html"
+rm -rf "$WORKDIR" "$FILE_PATH" && mkdir -p "$WORKDIR" "$FILE_PATH" && chmod 777 "$WORKDIR" "$FILE_PATH" >/dev/null 2>&1
+bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
+command -v curl &>/dev/null && COMMAND="curl -so" || command -v wget &>/dev/null && COMMAND="wget -qO" || { red "Error: neither curl nor wget found, please install one of them." >&2; exit 1; }
+
+check_port () {
+  clear
+  echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
+  port_list=$(devil port list)
+  tcp_ports=$(echo "$port_list" | grep -c "tcp")
+  udp_ports=$(echo "$port_list" | grep -c "udp")
+
+  if [[ $udp_ports -lt 1 ]]; then
+      echo -e "\e[1;91m没有可用的UDP端口,正在调整...\e[0m"
+
+      if [[ $tcp_ports -ge 3 ]]; then
+          tcp_port_to_delete=$(echo "$port_list" | awk '/tcp/ {print $1}' | head -n 1)
+          devil port del tcp $tcp_port_to_delete
+          echo -e "\e[1;32m已删除TCP端口: $tcp_port_to_delete\e[0m"
+      fi
+
+      while true; do
+          udp_port=$(shuf -i 10000-65535 -n 1)
+          result=$(devil port add udp $udp_port 2>&1)
+          if [[ $result == *"Ok"* ]]; then
+              echo -e "\e[1;32m已添加UDP端口: $udp_port"
+              udp_port1=$udp_port
+              break
+          else
+              echo -e "\e[1;33m端口 $udp_port 不可用，尝试其他端口...\e[0m"
+          fi
+      done
+
+      echo -e "\e[1;32m端口已调整完成,如安装完后节点不通,访问 /restart域名重启\e[0m"
+      devil binexec on >/dev/null 2>&1
+      kill -9 $(ps -o ppid= -p $$) >/dev/null 2>&1
+  else
+      udp_ports=$(echo "$port_list" | awk '/udp/ {print $1}')
+      udp_port1=$(echo "$udp_ports" | sed -n '1p')
+  fi
+
+  export PORT=$udp_port1
+  echo -e "\e[1;35mhysteria2使用udp端口: $udp_port1\e[0m"
+}
+check_port
+
+ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
+if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+    BASE_URL="https://github.com/eooce/test/releases/download/freebsd-arm64"
+elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+    BASE_URL="https://github.com/eooce/test/releases/download/freebsd"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+FILE_INFO=("$BASE_URL/hy2 web")
+
+declare -A FILE_MAP
+for entry in "${FILE_INFO[@]}"; do
+    URL=$(echo "$entry" | cut -d ' ' -f 1)
+    TYPE=$(echo "$entry" | cut -d ' ' -f 2)
+    
+    # 设置固定文件名
+    case $TYPE in
+        "web") FIXED_NAME="hysteria2" ;;
+        *) FIXED_NAME="$TYPE" ;;
+    esac
+    
+    NEW_FILENAME="$DOWNLOAD_DIR/$FIXED_NAME"
+    
+    $COMMAND "$NEW_FILENAME" "$URL"
+    echo -e "\e[1;32mDownloading $NEW_FILENAME\e[0m"
+    chmod +x "$NEW_FILENAME"
+    FILE_MAP[$TYPE]="$NEW_FILENAME"
+done
+wait
+
+# Generate cert
+openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout "$WORKDIR/server.key" -out "$WORKDIR/server.crt" -subj "/CN=${CURRENT_DOMAIN}" -days 36500
+
+get_ip() {
+  IP_LIST=($(devil vhost list | awk '/^[0-9]+/ {print $1}'))
+  API_URL="https://status.eooce.com/api"
+  IP=""
+  THIRD_IP=${IP_LIST[2]}
+  RESPONSE=$(curl -s --max-time 2 "${API_URL}/${THIRD_IP}")
+  if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
+      IP=$THIRD_IP
+  else
+      FIRST_IP=${IP_LIST[0]}
+      RESPONSE=$(curl -s --max-time 2 "${API_URL}/${FIRST_IP}")
+      
+      if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
+          IP=$FIRST_IP
+      else
+          IP=${IP_LIST[1]}
+      fi
+  fi
+echo "$IP"
+}
+
+echo -e "\e[1;32m获取可用IP中,请稍等...\e[0m"
+HOST_IP=$(get_ip)
+echo -e "\e[1;35m当前选择IP为: $HOST_IP 如安装完后节点不通可尝试重新安装\e[0m"
+
+cat << EOF > config.yaml
+listen: $HOST_IP:$PORT
+
+tls:
+  cert: "$WORKDIR/server.crt"
+  key: "$WORKDIR/server.key"
+
+auth:
+  type: password
+  password: "$UUID"
+
+fastOpen: true
+
+masquerade:
+  type: proxy
+  proxy:
+    url: https://bing.com
+    rewriteHost: true
+
+transport:
+  udp:
+    hopInterval: 30s
+EOF
+
+install_keepalive () {
+    echo -e "\n\e[1;35m正在安装保活服务中,请稍等......\e[0m"
+    devil www del keep.${USERNAME}.${CURRENT_DOMAIN} > /dev/null 2>&1
+    devil www add keep.${USERNAME}.${CURRENT_DOMAIN} nodejs /usr/local/bin/node18 > /dev/null 2>&1
+    keep_path="$HOME/domains/keep.${USERNAME}.${CURRENT_DOMAIN}/public_nodejs"
+    [ -d "$keep_path" ] || mkdir -p "$keep_path"
+    app_file_url="https://hy2.ssss.nyc.mn/hy2.js"
+    $COMMAND "${keep_path}/app.js" "$app_file_url" 
+
+    cat > ${keep_path}/.env <<EOF
+UUID=${UUID}
+SUB_TOKEN=${SUB_TOKEN}
+UPLOAD_URL=${UPLOAD_URL}
+TELEGRAM_CHAT_ID=${CHAT_ID}
+TELEGRAM_BOT_TOKEN=${BOT_TOKEN}
+EOF
+    devil www add ${USERNAME}.${CURRENT_DOMAIN} php > /dev/null 2>&1
+    index_url="https://github.com/eooce/Sing-box/releases/download/00/index.html"
+    [ -f "${FILE_PATH}/index.html" ] || $COMMAND "${FILE_PATH}/index.html" "$index_url"
+    ln -fs /usr/local/bin/node18 ~/bin/node > /dev/null 2>&1
+    ln -fs /usr/local/bin/npm18 ~/bin/npm > /dev/null 2>&1
+    mkdir -p ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    echo 'export PATH=~/.npm-global/bin:~/bin:$PATH' >> $HOME/.bash_profile && source $HOME/.bash_profile
+    rm -rf $HOME/.npmrc > /dev/null 2>&1
+    cd ${keep_path} && npm install dotenv axios --silent > /dev/null 2>&1
+    rm $HOME/domains/keep.${USERNAME}.${CURRENT_DOMAIN}/public_nodejs/public/index.html > /dev/null 2>&1
+    devil www restart keep.${USERNAME}.${CURRENT_DOMAIN} > /dev/null 2>&1
+    if curl -skL "http://keep.${USERNAME}.${CURRENT_DOMAIN}/${USERNAME}" | grep -q "running"; then
+        echo -e "\e[1;32m\n全自动保活服务安装成功\n\e[0m"
+	    echo -e "\e[1;32m所有服务都运行正常,全自动保活任务添加成功\n\n\e[0m"
+        echo -e "\e[1;35m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/restart 重启进程\n\e[0m"
+        echo -e "\e[1;35m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/list 全部进程列表\n\e[0m"
+        echo -e "\e[1;33m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/${USERNAME} 调起保活程序 备用: http://keep.${USERNAME}.${CURRENT_DOMAIN}/run\n\e[0m"
+        echo -e "\e[1;35m访问 http://keep.${USERNAME}.${CURRENT_DOMAIN}/status 查看进程状态\n\e[0m"
+        echo -e "\e[1;35m如果需要TG通知,在\e[1;33m https://t.me/laowang_serv00_bot \e[1;35m获取CHAT_ID,并带CHAT_ID环境变量运行\n\n\e[0m"
+    else
+        echo -e "\e[1;31m\n全自动保活服务安装失败,存在未运行的进程,访问 \e[1;33mhttp://keep.${USERNAME}.${CURRENT_DOMAIN}/status \e[1;31m检查,建议执行以下命令后重装: \n\ndevil www del ${USERNAME}.${CURRENT_DOMAIN}\ndevil www del keep.${USERNAME}.${CURRENT_DOMAIN}\nrm -rf $HOME/domains/*\nshopt -s extglob dotglob\nrm -rf $HOME/!(domains|mail|repo|backups)\n\n\e[0m"
+    fi
+}
+
+run() {
+  if [ -e "$(basename ${FILE_MAP[web]})" ]; then
+    nohup ./"$(basename ${FILE_MAP[web]})" server config.yaml >/dev/null 2>&1 &
+    sleep 1
+    pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) is running\e[0m" || { echo -e "\e[1;35m$(basename ${FILE_MAP[web]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" server config.yaml >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) restarted\e[0m"; }
+  fi
+
+  for key in "${!FILE_MAP[@]}"; do
+      if [ -e "$(basename ${FILE_MAP[$key]})" ]; then
+          rm -rf "$(basename ${FILE_MAP[$key]})" >/dev/null 2>&1
+      fi
+  done
+}
+run
+
+get_name() { if [ "$HOSTNAME" = "s1.ct8.pl" ]; then SERVER="CT8"; else SERVER=$(echo "$HOSTNAME" | cut -d '.' -f 1); fi; echo "$SERVER"; }
+NAME="$(get_name)-hysteria2-${USERNAME}"
+
+ISP=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "0")
+
+echo -e "\n\e[1;32mHysteria2安装成功\033[0m\n"
+echo -e "\e[1;33mV2rayN 或 Nekobox、小火箭等直接导入,跳过证书验证需设置为true\033[0m\n"
+cat > ${FILE_PATH}/${SUB_TOKEN}_hy2.log <<EOF
+hysteria2://$UUID@$HOST_IP:$PORT/?sni=www.bing.com&alpn=h3&insecure=1#$ISP-$NAME
+EOF
+cat ${FILE_PATH}/${SUB_TOKEN}_hy2.log
+echo -e "\n\e[1;35mClash: \033[0m"
+cat << EOF
+- name: $ISP-$NAME
+  type: hysteria2
+  server: $HOST_IP
+  port: $PORT
+  password: $UUID
+  alpn:
+    - h3
+  sni: www.bing.com
+  skip-cert-verify: true
+  fast-open: true
+EOF
+echo ""
+rm -rf config.yaml fake_useragent_0.2.0.json
+# install_keepalive
+echo -e "\e[1;35m老王serv00|CT8单协议hysteria2无交互一键安装脚本\e[0m"
+echo -e "\e[1;35m脚本地址: https://github.com/eooce/sing-box\e[0m"
+echo -e "\e[1;35m反馈论坛: https://bbs.vps8.me\e[0m"
+echo -e "\e[1;35mTG反馈群组: https://t.me/vps888\e[0m"
+echo -e "\e[1;35m转载请著名出处,请勿滥用\e[0m\n"
+echo -e "\e[1;32mRuning done!\033[0m\n"
