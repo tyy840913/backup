fe87bc4dd539bc56df09ce6e5a9f823b66b4f155
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2025-06-07 09:29:27 +0800

    Update auto_backup.sh

--- auto_backup.sh
+++ auto_backup.sh
@@ -7,7 +7,7 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 # 下载配置
-URL="https://alist.woskee.nyc.mn:88/AliCloud/backup/auto.sh"
+URL="https://alist.woskee.nyc.mn:88/dav/AliCloud/backup/auto.sh"
 TARGET="/root/auto.sh"
 
 # 交互式凭据输入
