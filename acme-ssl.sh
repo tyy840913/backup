6a785cd481ee847a2680b1833ebcab6e4ce4fa01
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2025-12-10 14:27:09 +0800

    更新 acme-ssl.sh

--- acme-ssl.sh
+++ acme-ssl.sh
@@ -17,7 +17,7 @@ init_directories() {
 
 # 检查并安装依赖
 check_dependencies() {
-    local dependencies=("openssl" "crontab")
+    local dependencies=("openssl")
     local missing=()
     
     echo -e "${BLUE}[*] 检查系统依赖...${NC}"
